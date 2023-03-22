35d24a2e12f6cce872337a40adbef1d595d6af02
siddharth9300 <jkrish930@gmail.com>
2023-03-22 20:32:57 +0530

    added border in todo item

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -7,22 +7,21 @@ export const TodoItem = ({todo ,onDelete}) => {
 
   return (
 
-<section className="text-gray-600 body-font overflow-hidden dark:bg-slate-900" id={"scroll-"+todo.sno}>
-  <div className="container  px-5 py-6 mx-auto">
+<section className="text-gray-600 my-4 border-gray-400 border-4 border-solid rounded-3xl body-font overflow-hidden dark:bg-slate-900" id={"scroll-"+todo.sno}>
+  <div className="container  px-5 py-2 mx-auto">
     <div className=" divide-y-2  divide-gray-100">
 
       <div className="py-8 flex flex-wrap md:flex-nowrap">
         <div className="md:w-16 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-          
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-red-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
             <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
             <path d="M22 4L12 14.01l-3-3"></path>
           </svg>
           {/* <span className="font-semibold text-2xl title-font text-gray-900" id={"sno"+todo.sno}>{todo.sno+1}</span> */}
         </div>
-        <div className="md:flex-grow">
+        <div className="md:flex-grow ">
           <h2 className="text-2xl font-medium dark:text-white text-gray-900 title-font mb-2" id={"title"+todo.sno}>{todo.title}</h2>
-          <p className="leading-relaxed dark:text-gray-200" id={"desc"+todo.sno}>{todo.desc}</p>
+          <p className="leading-relaxed dark:text-gray-200 break-words " id={"desc"+todo.sno}>{todo.desc}</p>
           <button className="flex text-white bg-red-500 border-0 py-2 px-6 items-center mt-4 focus:outline-none hover:bg-red-600 rounded del-btn" onClick={()=>{onDelete(todo)}}>Done</button>
         
         </div>
